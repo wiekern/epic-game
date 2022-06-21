a6f58b2c275f0265aae332fd8c66665c667fa241
wiekern <yanyafei0622@gmail.com>
2022-06-21 16:25:19 +0800

    deployed to rinkeby.

---
+++ scripts/deploy.js
@@ -0,0 +1,45 @@
+const main = async () => {
+    const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
+    const gameContract = await gameContractFactory.deploy(
+        ["Wukong", "Neza", "Pikachu"],       // Names
+        ["https://i.imgur.com/mOeGXrE.jpeg", // Images
+        "https://i.imgur.com/U3ECPSq.jpeg", 
+        "https://i.imgur.com/WMB6g9u.png"],
+        [100, 150, 220],                    // HP values
+        [100, 90, 70] 
+    );
+    await gameContract.deployed();
+    console.log("Contract deployed to:", gameContract.address);
+
+    let txn;
+    txn = await gameContract.mintCharacterNFT(0);
+    await txn.wait();
+    console.log("Minted NFT #1");
+
+    txn = await gameContract.mintCharacterNFT(1);
+    await txn.wait();
+    console.log("Minted NFT #2");
+
+    txn = await gameContract.mintCharacterNFT(2);
+    await txn.wait();
+    console.log("Minted NFT #3");
+
+    txn = await gameContract.mintCharacterNFT(1);
+    await txn.wait();
+    console.log("Minted NFT #4");
+
+    console.log("Done deploying and minting!");
+
+};
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+runMain();
